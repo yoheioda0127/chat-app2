1c6f913ceabe6904ab3e321c2e483d8983171b90
yoheioda0127 <maikemi00romans@gamil.com>
2021-03-27 13:17:43 +0900

    roomsテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validate :name,presence: true
-  
+  validates :name, presence: true
+
 end
