de0089e69139ce0cf959918ace26f43245f7516e
yoheioda0127 <maikemi00romans@gamil.com>
2021-03-28 18:08:52 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,29 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room    = Room.find(params[:room_id])
+    # paramsに含まれているroom_idを代入
+    # /rooms/:room_id/messagesといったパスになる
+    # パスにroom_idが含まれているため、paramsというハッシュオブジェクトの中に、room_idという値が存在。
+    # params[:room_id]と記述することでroom_idを取得可能。
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    # @room.newではない。これにより、チャットルームに紐づいたメッセージのインスタンスを生成
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    # (ログインしているユーザーのidと紐付いている)メッセージの内容contentを受け取れるように許可.
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'rooms#index'
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  #↓roomsの中でしか行われないアクションだからネストにする（チャットルームに入らないと行えないアクション）
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
